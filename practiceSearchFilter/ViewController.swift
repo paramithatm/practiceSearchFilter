ebedb81ff7654d6b911d6db71d3363bcfad7300d
Paramitha <paramitha@tokopedia.com>
2019-04-15 11:25:13 +0700

    Revert "add podfile"
    This reverts commit 5065569aef1cc6561a96434b4dd8f21af5406626.

--- practiceSearchFilter/ViewController.swift
+++ practiceSearchFilter/ViewController.swift
@@ -10,11 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var searchTF: UITextField!
-    @IBOutlet weak var searchBtn: UIButton!
-
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
