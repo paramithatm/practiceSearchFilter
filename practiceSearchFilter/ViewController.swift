5065569aef1cc6561a96434b4dd8f21af5406626
Paramitha <paramitha@tokopedia.com>
2019-04-15 11:24:06 +0700

    add podfile

--- practiceSearchFilter/ViewController.swift
+++ practiceSearchFilter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchTF: UITextField!
+    @IBOutlet weak var searchBtn: UIButton!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
